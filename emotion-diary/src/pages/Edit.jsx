64828374f2bc1199216b494a3015b05127f8158b
Hayoung Shim <gkdud3579@gmail.com>
2024-03-08 12:53:43 +0900

    Chore: 파비콘 및 썸네일추가

--- emotion-diary/src/pages/Edit.jsx
+++ emotion-diary/src/pages/Edit.jsx
@@ -5,16 +5,20 @@ import Editor from "../components/Editor";
 import { useContext, useEffect, useState } from "react";
 import { DiaryDispatchContext, DiaryStateContext } from "../App";
 import useDiary from "../hooks/useDiary";
+import usePageTitle from "../hooks/usePageTitle";
 
 const Edit = () => {
   const params = useParams();
   const nav = useNavigate();
   const { onDelete, onUpdate } = useContext(DiaryDispatchContext);
   const curDiaryItem = useDiary(params.id);
+  usePageTitle(`${params.id}번 일기 수정`);
 
   const onClickDelete = () => {
-    if (window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않습니다!")) {
-      //일기 삭제 로직
+    if (
+      window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않아요!")
+    ) {
+      // 일기 삭제 로직
       onDelete(params.id);
       nav("/", { replace: true });
     }
@@ -36,9 +40,15 @@ const Edit = () => {
     <div>
       <Header
         title={"일기 수정하기"}
-        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
+        leftChild={
+          <Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />
+        }
         rightChild={
-          <Button onClick={onClickDelete} text={"삭제하기"} type={"NEGATIVE"} />
+          <Button
+            onClick={onClickDelete}
+            text={"삭제하기"}
+            type={"NEGATIVE"}
+          />
         }
       />
       <Editor initData={curDiaryItem} onSubmit={onSubmit} />
--- emotion-diary/src/pages/New.jsx
+++ emotion-diary/src/pages/New.jsx
@@ -2,15 +2,21 @@ import Header from "../components/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
 import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { DiaryDispatchContext } from "../App";
+import usePageTitle from "../hooks/usePageTitle";
 
 const New = () => {
   const { onCreate } = useContext(DiaryDispatchContext);
   const nav = useNavigate();
+  usePageTitle("새 일기 쓰기");
 
   const onSubmit = (input) => {
-    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    onCreate(
+      input.createdDate.getTime(),
+      input.emotionId,
+      input.content
+    );
     nav("/", { replace: true });
   };
 
@@ -18,7 +24,9 @@ const New = () => {
     <div>
       <Header
         title={"새 일기 쓰기"}
-        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
+        leftChild={
+          <Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />
+        }
       />
       <Editor onSubmit={onSubmit} />
     </div>
